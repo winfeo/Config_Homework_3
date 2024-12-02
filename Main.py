acc18a7acef3df94b09c821cf781f17c313b2b51
Kirill <kirill.ryabikin.05@gmail.com>
2024-12-02 13:37:24 +0300

    Разработка первоначальных файлов

--- Main.py
+++ Main.py
@@ -1,41 +1,146 @@
 import argparse
 import xml.etree.ElementTree as ET
 
-# Lexer implementation
+# Лексический анализатор (токенизатор)
 class Lexer:
     def __init__(self, text):
         self.text = text
         self.pos = 0
-        # Initialize lexer components
+        self.tokens = []
 
     def tokenize(self):
-        # Tokenize the input text
-        pass
+        while self.pos < len(self.text):
+            if self.text[self.pos:self.pos+2] == '#=':
+                self.pos += 2
+                while self.text[self.pos:self.pos+2] != '=#':
+                    self.pos += 1
+                self.pos += 2
+            elif self.text[self.pos].isspace():
+                self.pos += 1
+            elif self.text[self.pos].isdigit() or self.text[self.pos] == '.':
+                start = self.pos
+                while self.pos < len(self.text) and (self.text[self.pos].isdigit() or self.text[self.pos] == '.'):
+                    self.pos += 1
+                self.tokens.append(('NUMBER', self.text[start:self.pos]))
+            elif self.text[self.pos].isalpha():
+                start = self.pos
+                while self.pos < len(self.text) and self.text[self.pos].isalpha():
+                    self.pos += 1
+                self.tokens.append(('IDENTIFIER', self.text[start:self.pos]))
+            else:
+                self.tokens.append((self.text[self.pos], self.text[self.pos]))
+                self.pos += 1
+        return self.tokens
 
-# Parser implementation
+# Синтаксический анализатор (парсер)
 class Parser:
     def __init__(self, tokens):
         self.tokens = tokens
         self.pos = 0
-        # Initialize parser components
+        self.variables = {}
 
     def parse(self):
-        # Build AST from tokens
-        pass
+        self.parse_statements()
+        return self.variables
 
-# Evaluator implementation
-class Evaluator:
-    def evaluate_postfix(self, expression):
-        # Evaluate postfix expressions
-        pass
+    def parse_statements(self):
+        while self.pos < len(self.tokens):
+            if self.tokens[self.pos][0] == 'var':
+                self.parse_variable_declaration()
+            elif self.tokens[self.pos][0] == '{':
+                self.parse_dictionary()
+            else:
+                raise SyntaxError(f"Unexpected token: {self.tokens[self.pos]}")
 
-# XML Generator implementation
+    def parse_variable_declaration(self):
+        self.pos += 1
+        if self.tokens[self.pos][0] != 'IDENTIFIER':
+            raise SyntaxError("Expected identifier after 'var'")
+        name = self.tokens[self.pos][1]
+        self.pos += 1
+        if self.tokens[self.pos][0] != '=':
+            raise SyntaxError("Expected '=' after identifier")
+        self.pos += 1
+        value = self.parse_value()
+        if self.tokens[self.pos][0] != ';':
+            raise SyntaxError("Expected ';' after value")
+        self.pos += 1
+        self.variables[name] = value
+
+    def parse_value(self):
+        if self.tokens[self.pos][0] == 'NUMBER':
+            return float(self.tokens[self.pos][1])
+        elif self.tokens[self.pos][0] == 'IDENTIFIER':
+            return self.variables[self.tokens[self.pos][1]]
+        elif self.tokens[self.pos][0] == '@':
+            return self.parse_postfix_expression()
+        elif self.tokens[self.pos][0] == '{':
+            return self.parse_dictionary()
+        else:
+            raise SyntaxError(f"Unexpected token: {self.tokens[self.pos]}")
+
+    def parse_postfix_expression(self):
+        self.pos += 2  # Skip '@['
+        stack = []
+        while self.tokens[self.pos][0] != ']':
+            if self.tokens[self.pos][0] == 'IDENTIFIER':
+                stack.append(self.variables[self.tokens[self.pos][1]])
+            elif self.tokens[self.pos][0] == 'NUMBER':
+                stack.append(float(self.tokens[self.pos][1]))
+            elif self.tokens[self.pos][0] in '+-*/':
+                b = stack.pop()
+                a = stack.pop()
+                if self.tokens[self.pos][0] == '+':
+                    stack.append(a + b)
+                elif self.tokens[self.pos][0] == '-':
+                    stack.append(a - b)
+                elif self.tokens[self.pos][0] == '*':
+                    stack.append(a * b)
+                elif self.tokens[self.pos][0] == '/':
+                    stack.append(a / b)
+            elif self.tokens[self.pos][0] == 'mod':
+                b = stack.pop()
+                a = stack.pop()
+                stack.append(a % b)
+            self.pos += 1
+        self.pos += 1  # Skip ']'
+        return stack[0]
+
+    def parse_dictionary(self):
+        self.pos += 1  # Skip '{'
+        dictionary = {}
+        while self.tokens[self.pos][0] != '}':
+            if self.tokens[self.pos][0] != 'IDENTIFIER':
+                raise SyntaxError("Expected identifier in dictionary")
+            key = self.tokens[self.pos][1]
+            self.pos += 1
+            if self.tokens[self.pos][0] != ':':
+                raise SyntaxError("Expected ':' after identifier in dictionary")
+            self.pos += 1
+            value = self.parse_value()
+            dictionary[key] = value
+            if self.tokens[self.pos][0] == ',':
+                self.pos += 1
+        self.pos += 1  # Skip '}'
+        return dictionary
+
+# Генератор XML
 class XMLGenerator:
-    def generate_xml(self, ast):
-        # Convert AST to XML
-        pass
+    def generate_xml(self, variables):
+        root = ET.Element("config")
+        self.add_dictionary(root, variables)
+        return ET.tostring(root, encoding='unicode')
 
-# Main function
+    def add_dictionary(self, parent, dictionary):
+        dict_element = ET.SubElement(parent, "dictionary")
+        for key, value in dictionary.items():
+            entry_element = ET.SubElement(dict_element, "entry", name=key)
+            if isinstance(value, dict):
+                self.add_dictionary(entry_element, value)
+            else:
+                entry_element.set("value", str(value))
+
+# Основная функция
 def main():
     parser = argparse.ArgumentParser(description='Configuration Language to XML Translator')
     parser.add_argument('input_file', help='Path to the input configuration file')
@@ -48,13 +153,10 @@ def main():
     tokens = lexer.tokenize()
 
     parser = Parser(tokens)
-    ast = parser.parse()
-
-    evaluator = Evaluator()
-    # Evaluate expressions in AST
+    variables = parser.parse()
 
     xml_generator = XMLGenerator()
-    xml_output = xml_generator.generate_xml(ast)
+    xml_output = xml_generator.generate_xml(variables)
 
     print(xml_output)
 
